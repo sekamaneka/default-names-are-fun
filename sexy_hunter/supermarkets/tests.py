a2b49231da4537ac90c816038c804873107618d2
swilso793 <swilso793@gmail.com>
2015-01-31 01:32:04 +0100

    added the new home page template

--- sexy_hunter/supermarkets/tests.py
+++ sexy_hunter/supermarkets/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 from supermarkets.views import supermarket_list
+
 
 class HomePageTest(TestCase):
     
@@ -12,7 +14,8 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpResponse()
         response = supermarket_list(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
 # Create your tests here.
 
 
