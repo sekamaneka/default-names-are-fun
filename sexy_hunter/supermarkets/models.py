55b375e7467ee2cc30e7b7f38d651a17b5efab52
swilso793 <swilso793@gmail.com>
2015-02-01 01:57:50 +0100

    added a new view. I have to create the url mapping and the template that will show the post form

--- sexy_hunter/supermarkets/models.py
+++ sexy_hunter/supermarkets/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+#we got a class for the supermarkets
+#which has a list of products
 
+class Supermarket(models.Model):
+    name = models.CharField(max_length = 30)
+    opening_hours = models.CharField(max_length = 100)
+    pass
+
+
+class Item(models.Model):
+    supermarket = models.ForeignKey(Supermarket)
+    name = models.CharField(max_length = 50)
+    price = models.CharField(max_length = 30)
+    
 # Create your models here.
--- sexy_hunter/supermarkets/views.py
+++ sexy_hunter/supermarkets/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from supermarkets.models import Item
+
 
 def supermarket_list(request):
-    return render(request, 'home.html')
+    all_of_the_items = Item.objects.order_by('supermarket')  
+    return render(request, 'home.html', { all_of_the_items : 'items'})
+
+def add_supermarket(request):
+    new_market_name = request.POST['supermarket_name']
+    new_market_opening_hours = request.POST['opening_hours']
+    supermarket = Supermarket.objects.create(new_market_opening_hours, new_market_name)
+    return render(request, 'add_supermarket.html',)
 # Create your views here.
